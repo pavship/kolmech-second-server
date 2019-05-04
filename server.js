04a3e57cf066432725f09b9902801368dd62a821
Pavel Shipitsyn <pavship@yandex.ru>
2019-05-04 03:24:35 +0300

    + basic POST requests handling

--- server.js
+++ server.js
@@ -1,11 +1,21 @@
 const express = require('express')
 const app = express()
 
-let counter = 0
+app.use(express.json())
+
+let getCounter = 1
+let postCounter = 1
 
 app.get('/', (req, res) => {
-  console.log('received request > ' + counter)
+  console.log('received GET request > ' + getCounter++)
   res.send('Hello World!')
+})
+
+app.post('/', (req, res) => {
+  console.log('received POST request > ' + postCounter++)
+  const body = req.body
+  console.log('body > ', body)
+  res.status(200).send('Request handled')
 })
 
 const port = process.env.PORT || 8000
