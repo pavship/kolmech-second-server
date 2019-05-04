49c5c1885e79a0c71c77e11d11bad569d068922e
Pavel Shipitsyn <pavship@yandex.ru>
2019-05-04 12:51:29 +0300

    wrapped into try catch

--- server.js
+++ server.js
@@ -26,43 +26,51 @@ app.get('/', (req, res) => {
 })
 
 app.post('/lead/status', async (req, res) => {
-  res.status(200).send('Request handled')
-  const body = req.body
-  console.log('/lead/status req body > ', JSON.stringify(body, null, 2))
-  const deal = body.leads.status[0]
-  console.log('deal > ', deal)
-  if (deal.status_id === skipStatusId) return console.log('skipped <')
-  const createdLocalDate = new Date(parseInt(deal.date_create + '000', 10)+180*60000).toISOString().slice(0,10)
-  const { statusText: createFolderStatusText } = await disk.put('?'+
-    qs.stringify({
-      path: `${dealsDirPath}${createdLocalDate}_${deal.name}_${deal.id}`,
-    })
-  )
-  console.log('createFolderStatusText > ', createFolderStatusText)
+  try {
+    res.status(200).send('Request handled')
+    const body = req.body
+    console.log('/lead/status req body > ', JSON.stringify(body, null, 2))
+    const deal = body.leads.status[0]
+    console.log('deal > ', deal)
+    if (deal.status_id === skipStatusId) return console.log('skipped <')
+    const createdLocalDate = new Date(parseInt(deal.date_create + '000', 10)+180*60000).toISOString().slice(0,10)
+    const { statusText: createFolderStatusText } = await disk.put('?'+
+      qs.stringify({
+        path: `${dealsDirPath}${createdLocalDate}_${deal.name}_${deal.id}`,
+      })
+    )
+    console.log('createFolderStatusText > ', createFolderStatusText)
+  } catch (err) {
+    console.log('err.message > ', err.message)
+  }
 })
 
 app.post('/lead/update', async (req, res) => {
-  res.status(200).send('Request handled')
-  const body = req.body
-  console.log('/lead/update req body > ', JSON.stringify(body, null, 2))
-  const deal = body.leads.update[0]
-  const createdLocalDate = new Date(parseInt(deal.date_create + '000', 10)+180*60000).toISOString().slice(0,10)
-  const { data: { _embedded: { items: resources }}} = await disk.get('?'+
-    qs.stringify({ path: dealsDirPath })
-  )
-  const dirNames = resources.filter(r => r.type === 'dir').map(({ name }) => name)
-  console.log('dirNames > ', dirNames)
-  const oldDirName = dirNames.find(n => n.slice(-deal.id.length) === deal.id)
-  console.log('oldDirName > ', oldDirName)
-  const newName = `${createdLocalDate}_${deal.name}_${deal.id}`
-  console.log('newName > ', newName)
-  const { statusText: renameFolderStatusText } = await disk.post('/move?'+
-    qs.stringify({
-      from: `${dealsDirPath}${oldDirName}`,
-      path: `${dealsDirPath}${newName}`,
-    })
-  )
-  console.log('renameFolderStatusText > ', renameFolderStatusText)
+  try {
+    res.status(200).send('Request handled')
+    const body = req.body
+    console.log('/lead/update req body > ', JSON.stringify(body, null, 2))
+    const deal = body.leads.update[0]
+    const createdLocalDate = new Date(parseInt(deal.date_create + '000', 10)+180*60000).toISOString().slice(0,10)
+    const { data: { _embedded: { items: resources }}} = await disk.get('?'+
+      qs.stringify({ path: dealsDirPath })
+    )
+    const dirNames = resources.filter(r => r.type === 'dir').map(({ name }) => name)
+    console.log('dirNames > ', dirNames)
+    const oldDirName = dirNames.find(n => n.slice(-deal.id.length) === deal.id)
+    console.log('oldDirName > ', oldDirName)
+    const newName = `${createdLocalDate}_${deal.name}_${deal.id}`
+    console.log('newName > ', newName)
+    const { statusText: renameFolderStatusText } = await disk.post('/move?'+
+      qs.stringify({
+        from: `${dealsDirPath}${oldDirName}`,
+        path: `${dealsDirPath}${newName}`,
+      })
+    )
+    console.log('renameFolderStatusText > ', renameFolderStatusText)
+  } catch (err) {
+    console.log('err.message > ', err.message)
+  }
 })
 
 const port = process.env.PORT || 8000
