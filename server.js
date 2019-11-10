0c1947257b0d8fb43c8b7b5512e69c394b1a32ba
PS <pavship@yandex.ru>
2019-11-10 16:06:30 +0300

    start modularization for future extension

--- server.js
+++ server.js
@@ -1,53 +1,15 @@
 const express = require('express')
 const app = express()
-
 require('dotenv').config()
-
 const qs = require('qs')
 app.use(express.json())
 app.use(express.urlencoded())
 
-const axios = require('axios')
+const { disk, getFolderName, getDiskResources, getDiskResources2Levels } = require('./src/disk')
 
-const disk = axios.create({
-	baseURL: 'https://cloud-api.yandex.net/v1/disk/resources',
-	headers: {
-		'content-type': 'application/json',
-		'Authorization': 'OAuth ' + process.env.DOCS_KOLMECH_TOKEN,
-	}
-})
 const dealsDirPath = '/Заявки ХОНИНГОВАНИЕ.РУ'
 
 const skipStatusId = '24659131' //leads with this (PROJECTS) status are not handled
-
-const getDiskResources = async path => {
-	const { data: { _embedded: { items: resources }}} = await disk.get('?'+
-		qs.stringify({ path, limit: 10000 }))
-	return resources
-}
-
-const getFolderName = async (path, id) => {
-	const dirFoldersNames = (await getDiskResources(path))
-		.filter(r => r.type === 'dir').map(({ name }) => name)
-	return dirFoldersNames.find(n => n.slice(-id.length) === id)
-}
-
-const getDiskResources2Levels = async path => {
-	const level1 = (await getDiskResources(path))
-		.filter(r => r.type === 'dir')
-		.map(({ name }) => name)
-	const level2 = await Promise.all(level1.map(folder => getDiskResources(`${path}/${folder}`)))
-	const resources = []
-	level2.forEach((rs, i) => {
-		rs.filter(r => r.type === 'dir')
-			.forEach(r => resources.push({
-				name: r.name,
-				id: r.name.slice(r.name.lastIndexOf('_') + 1),
-				parent: level1[i]
-			}))
-	})
-	return resources
-}
 
 // const deal = {
 // 	id: "164837",
@@ -95,7 +57,7 @@ app.post('/lead/update', async (req, res) => {
 		const deal = req.body.leads.add
 			? req.body.leads.add[0]
 			: req.body.leads.update[0]
-		// console.log('/lead/update deal > ', deal)
+		console.log('/lead/update deal > ', deal)
 		if (deal.status_id === skipStatusId) return console.log('skipped project type deal <')
 		const oldStatusFolderName = await getFolderName(dealsDirPath, deal.old_status_id)
 		const newStatusFolderName = await getFolderName(dealsDirPath, deal.status_id)
@@ -128,7 +90,7 @@ app.post('/lead/delete', async (req, res) => {
 	try {
 		res.status(200).send('Request handled')
 		const deal = req.body.leads.delete[0]
-		// console.log('/lead/delete deal > ', deal)
+		console.log('/lead/delete deal > ', deal)
 		const resourses = await getDiskResources2Levels(dealsDirPath)
 		const resourse = resourses.find(r => r.id === deal.id)
 		if (!resourse) throw new Error('Не найдена папка удаляемой сделки # ' + deal.id)
---
+++ src/disk.js
@@ -0,0 +1,45 @@
+const axios = require('axios')
+
+const disk = axios.create({
+	baseURL: 'https://cloud-api.yandex.net/v1/disk/resources',
+	headers: {
+		'content-type': 'application/json',
+		'Authorization': 'OAuth ' + process.env.DOCS_KOLMECH_TOKEN,
+	}
+})
+
+const getDiskResources = async path => {
+	const { data: { _embedded: { items: resources }}} = await disk.get('?'+
+		qs.stringify({ path, limit: 10000 }))
+	return resources
+}
+
+const getFolderName = async (path, id) => {
+	const dirFoldersNames = (await getDiskResources(path))
+		.filter(r => r.type === 'dir').map(({ name }) => name)
+	return dirFoldersNames.find(n => n.slice(-id.length) === id)
+}
+
+const getDiskResources2Levels = async path => {
+	const level1 = (await getDiskResources(path))
+		.filter(r => r.type === 'dir')
+		.map(({ name }) => name)
+	const level2 = await Promise.all(level1.map(folder => getDiskResources(`${path}/${folder}`)))
+	const resources = []
+	level2.forEach((rs, i) => {
+		rs.filter(r => r.type === 'dir')
+			.forEach(r => resources.push({
+				name: r.name,
+				id: r.name.slice(r.name.lastIndexOf('_') + 1),
+				parent: level1[i]
+			}))
+	})
+	return resources
+}
+
+module.exports = { 
+  disk,
+  getDiskResources,
+  getFolderName,
+  getDiskResources2Levels
+}
